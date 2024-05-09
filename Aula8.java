048c88bfb84982af2e684717121b7e9ef9ea4b3a
HerickMarlon <161661788+WoperWips@users.noreply.github.com>
2024-05-08 21:43:54 -0300

    Update Aula8.java

--- Aula8.java
+++ Aula8.java
@@ -1,37 +1,44 @@
 import java.util.Scanner;
 
-public class Aula8 {
+public class aula8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Digite quantas notas são necessária: ");
-         double numNotas = scanner.nextDouble();
+        System.out.print("Digite quantas notas sao necessária: ");
+         int numNotas = scanner.nextInt();
 
          double somaNotas = 0;
          double somaPesos = 0;
 
         for (int i = 1; i <= numNotas; i++){
-            System.out.printf("Digite a nota d% (em %): ", i);
+            System.out.println("Digite as notas:");
             double nota = scanner.nextDouble();
 
-            System.out.printf("Digite o peso da nota %d (em %):", i);
+            System.out.println("Digite os pesos das notas: ");
             double peso = scanner.nextDouble();
 
             somaNotas += (nota * peso);
             somaPesos += peso;
         
-        if (somaPesos > 100){
-            System.out.println("Error: A soma não pode ser maior que 100%. Por Favor Reniciar o Progama");
-            
-            scanner.close();
-            return;
-        }
+            if (somaPesos > 100){
+                System.out.println("Error: A soma não pode ser maior que 100%. Por Favor Reniciar o Progama" );
+                
+                scanner.close();
+                return;
+            }
+
+            if(){
+                
+            }
 
         }
-            double notaFinal = (somaNotas /  somaPesos);
-            System.out.printf("Nota final do aluno: %.2f\n",notaFinal);
 
-            scanner.close();
+        double notaFinal = (somaNotas / somaPesos );
+        System.out.println("A nota final e:"  + notaFinal);
+
+
+
+        scanner.close();
 
      }    
 }
