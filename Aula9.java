3396c39b7076df3d0d79721e5b4288518b18db12
HerickMarlon <161661788+WoperWips@users.noreply.github.com>
2024-05-14 15:23:32 -0300

    Update Aula9.java

--- Aula9.java
+++ Aula9.java
@@ -1,49 +1,58 @@
 import java.util.Scanner;
 
 public class Aula9 {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-    
-    System.out.print(" Quantos alunos deseja calcular a nota? ");
-    int quantidadesdeAlunos = scanner.nextInt();
-    scanner.nextLine();
-    
-    for (int i = 1; i <= quantidadesdeAlunos; i++) {
-        System.out.print(" Digite o nome do aluno: ");
-        String nome = scanner.nextLine();
 
-        System.out.print(" Quantas notas sera inseridas: " + nome + " ? " );
-        int quantidadeNotas = scanner.nextInt();
+        System.out.print("Quantos alunos terão suas notas calculadas? ");
+        int numAlunos = scanner.nextInt();
         scanner.nextLine();
 
-        double somanotas = 0;
-        double somapesos = 0;
+        for (int i = 0; i < numAlunos; i++) {
+            System.out.print("Digite o nome do aluno: ");
+            String nomeAluno = scanner.nextLine();
 
-        for (int j = 0; j < quantidadeNotas; j++) {
-            System.out.print(" Digite a nota: " );
-            double nota = scanner.nextDouble();
+            
+            System.out.print("Quantas notas serão inseridas para " + nomeAluno + "? ");
+            int numNotas = scanner.nextInt();
 
-            System.out.print("Digite o peso da nota: ");
-            double peso = scanner.nextDouble();
+            double somaNotas = 0;
+            double somaPesos = 0;
 
-            somanotas += nota * peso;
-            somapesos += peso;
-        
-        double mediaFinal = somanotas / somapesos;
+            for (int j = 0; j < numNotas; j++) {
+                
+                System.out.print("Digite a nota: ");
+                double nota = scanner.nextDouble();
 
-        System.out.println(" Alunos: " + nome);
-        System.out.println(" Média final: " + mediaFinal);
-}
+                
+                System.out.print("Digite o peso da nota: ");
+                double peso = scanner.nextDouble();
 
-        if (mediaFinal >= 0 && mediaFinal <= 2) {
-            System.out.println(" Condição: Reprovado ");
-        } else if (mediaFinal > 2 && mediaFinal <= 4.9) {
-            System.out.println(" Condição: Substitutiva ");
-        } else {
-            System.out.println("Condição: Aprovado");
+                
+                somaNotas += nota * peso;
+                somaPesos += peso;
+            }
+
+            
+            double mediaFinal = somaNotas / somaPesos;
+
+            String condicao;
+            if (mediaFinal >= 0 && mediaFinal <= 2) {
+                condicao = "Reprovado";
+            } else if (mediaFinal > 2 && mediaFinal <= 4.9) {
+                condicao = "Substitutiva";
+            } else {
+                condicao = "Aprovado";
+            }
+
+            
+            System.out.println("Nome do aluno: " + nomeAluno);
+            System.out.println("Média final: " + mediaFinal);
+            System.out.println("Condição: " + condicao);
+
         }
-        System.out.println();
-    }
+
         scanner.close();
     }
 }
