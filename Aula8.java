9702aa11e928dd004874d3495a5741b51063553d
HerickMarlon <161661788+WoperWips@users.noreply.github.com>
2024-05-15 15:14:38 -0300

    Update Aula8.java

--- Aula8.java
+++ Aula8.java
@@ -1,40 +1,51 @@
 import java.util.Scanner;
 
-public class aula8 {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Digite quantas notas sao necessária: ");
-         int numNotas = scanner.nextInt();
+        System.out.print("Digite quantas notas são necessárias: ");
+        int numNotas = scanner.nextInt();
 
-         double somaNotas = 0;
-         double somaPesos = 0;
+        double somaNotas = 0;
+        double somaPesos = 0;
 
-        for (int i = 1; i <= numNotas; i++){
-            System.out.println("Digite as notas:");
+        for (int i = 1; i <= numNotas; i++) {
+            System.out.print("Digite a nota " + i + ": ");
             double nota = scanner.nextDouble();
 
-            System.out.println("Digite os pesos das notas: ");
+            System.out.print("Digite o peso da nota " + i + ": ");
             double peso = scanner.nextDouble();
 
             somaNotas += (nota * peso);
             somaPesos += peso;
-        
-            if (somaPesos > 100){
-                System.out.println("Error: A soma não pode ser maior que 100%. Por Favor Reniciar o Progama" );
-                
+
+            if (somaPesos > 100) {
+                System.out.println("Erro: A soma dos pesos não pode ser maior que 100%. Por favor, reinicie o programa.");
                 scanner.close();
                 return;
             }
+            
+        double notaFinal = somaNotas / somaPesos;
+        System.out.println("A nota final é: " + notaFinal);
 
+        System.out.print("Qual é a média mínima: ");
+        double mediaMinima = scanner.nextDouble();
+
+        if (notaFinal >= 0 && notaFinal <= 10) {
+            if (notaFinal < 2) {
+                System.out.println("Aluno está reprovado.");
+            } else if (notaFinal >= 2 && notaFinal <= 4.9) {
+                System.out.println("Aluno precisa fazer substitutiva.");
+            } else if (notaFinal >= 5 && notaFinal < mediaMinima) {
+                System.out.println("Aluno está reprovado.");
+            } else if (notaFinal >= mediaMinima) {
+                System.out.println("Aluno está aprovado.");
+            }
+        } else {
+            System.out.println("Nota inválida. Por favor, digite notas e pesos válidos.");
         }
 
-        double notaFinal = (somaNotas / somaPesos );
-        System.out.println("A nota final e:"  + notaFinal);
-
-        
-
         scanner.close();
-
-     }    
+    }
 }
