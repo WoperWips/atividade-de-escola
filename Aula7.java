f8f46d112dd1ac1d6154224e67448383c88783e0
HerickMarlon <161661788+WoperWips@users.noreply.github.com>
2024-05-15 14:59:11 -0300

    Update Aula7.java

--- Aula7.java
+++ Aula7.java
@@ -1,17 +1,24 @@
 import java.util.Scanner;
 
-public class Main {
+public class Aula7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Qual é a média mínima: ");
+        double mediaMinima = scanner.nextDouble();
 
         System.out.print("Digite a nota do aluno: ");
         double nota = scanner.nextDouble();
 
-        if (nota >= 0 && nota <= 2) {
-            System.out.println("Aluno está reprovado.");
-        } else if (nota > 2 && nota <= 4.9) {
-            System.out.println("Aluno precisa fazer substitutiva.");
-        } else if (nota > 4.9) {
+        if (nota >= 0 && nota < mediaMinima) {
+            if (nota <= 2) {
+                System.out.println("Aluno está reprovado.");
+            } else if (nota > 2 && nota <= 4.9) {
+                System.out.println("Aluno precisa fazer substitutiva.");
+            } else {
+                System.out.println("Aluno está reprovado.");
+            }
+        } else if (nota >= mediaMinima && nota <= 10) {
             System.out.println("Aluno está aprovado.");
         } else {
             System.out.println("Nota inválida. Por favor, digite uma nota entre 0 e 10.");
